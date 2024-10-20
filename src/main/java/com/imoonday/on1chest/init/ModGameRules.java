6df4ede545d1f13f892b1e09264272eab11824ac
iMoonDay <562613736@qq.com>
2024-10-20 20:48:48 +0800

    修复GameRule注册问题，添加模组描述

--- src/main/java/com/imoonday/on1chest/init/ModGameRules.java
+++ src/main/java/com/imoonday/on1chest/init/ModGameRules.java
@@ -1,10 +1,12 @@
 package com.imoonday.on1chest.init;
 
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.GameRules;
 
 public class ModGameRules {
 
-    public static final GameRules.Key<GameRules.IntRule> MAX_MEMORY_RANGE = GameRules.register("maxMemoryRange", GameRules.Category.MISC, GameRules.IntRule.create(64));
+    public static final GameRules.Key<GameRules.IntRule> MAX_MEMORY_RANGE = GameRuleRegistry.register("maxMemoryRange", GameRules.Category.MISC, GameRuleFactory.createIntRule(64, 0));
 
     public static void register() {
 
